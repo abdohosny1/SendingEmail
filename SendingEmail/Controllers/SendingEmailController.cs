715f37875c2833017bef8fb9152605a12220420d
abdohosny1 <make.y90@yahoo.com>
2022-11-23 14:38:33 +0200

    add temp email

--- SendingEmail/Controllers/SendingEmailController.cs
+++ SendingEmail/Controllers/SendingEmailController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SendingEmail.DTO;
 using SendingEmail.services.sendingEmail;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace SendingEmail.Controllers
@@ -24,6 +25,20 @@ namespace SendingEmail.Controllers
         public async Task<IActionResult> sendEmail([FromForm] MailRequestDTO mailRequestDTO)
         {
             await _mailService.SendingEmail(mailRequestDTO.tOEmail, mailRequestDTO.Subject,mailRequestDTO.Body, mailRequestDTO.Attachment);
+            return Ok();
+        }
+        [HttpPost("welcome")]
+        public async Task<IActionResult> SendWelcomeEmail([FromBody] WelcomeRequestDto dto)
+        {
+            var filePath = $"{Directory.GetCurrentDirectory()}\\Templates\\EmailTemplate.html";
+            var str = new StreamReader(filePath);
+
+            var mailText = str.ReadToEnd();
+            str.Close();
+
+            mailText = mailText.Replace("[username]", dto.UserName).Replace("[email]", dto.Email);
+
+            await _mailService.SendingEmail(dto.Email, "Welcome to our channel", mailText);
             return Ok();
         }
 
