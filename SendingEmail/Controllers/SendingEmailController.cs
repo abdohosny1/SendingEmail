28b5fc27f8790de7d03550663b615bcf7f4f793b
abdohosny1 <make.y90@yahoo.com>
2022-11-23 14:06:43 +0200

    Add project files.

---
+++ SendingEmail/Controllers/SendingEmailController.cs
@@ -0,0 +1,31 @@
+﻿using MailKit;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SendingEmail.DTO;
+using SendingEmail.services.sendingEmail;
+using System.Threading.Tasks;
+
+namespace SendingEmail.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SendingEmailController : ControllerBase
+    {
+
+        private readonly IMailSendService _mailService;
+
+        public SendingEmailController(IMailSendService mailService)
+        {
+            _mailService = mailService;
+        }
+
+        [HttpPost("send")]
+
+        public async Task<IActionResult> sendEmail([FromForm] MailRequestDTO mailRequestDTO)
+        {
+            await _mailService.SendingEmail(mailRequestDTO.tOEmail, mailRequestDTO.Subject,mailRequestDTO.Body, mailRequestDTO.Attachment);
+            return Ok();
+        }
+
+    }
+}
---
+++ SendingEmail/services/sendingEmail/IMailSendService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SendingEmail.services.sendingEmail
+{
+    public interface IMailSendService
+    {
+        Task SendingEmail(string mailTo, string subject, string body,IList<IFormFile> ATTACHMENT =null);
+    }
+}
